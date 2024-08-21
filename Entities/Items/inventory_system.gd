9c8d2743331a238fef17f33c62e58dd0f76ec6da
wscalf <wscalf@gmail.com>
2024-08-21 14:55:39 -0400

    Add inventory system

---
+++ Entities/Items/inventory_system.gd
@@ -0,0 +1,18 @@
+extends Node
+
+signal item_added(item: Item)
+signal item_removed(item: Item)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func add_item(item: Item):
+	emit_signal("item_added", item)
+	
+func remove_item(item: Item):
+	emit_signal("item_removed", item)
+
+# Attempts to combine the given items, return true if successful, otherwise false, and emits signals for any changes to inventory content
+func combine_items(first: Item, second: Item) -> bool:
+	return false
