dd50f0cb6791657929428b1536631351e61e0dff
Stephen <tsadams@gmail.com>
2024-08-26 20:16:51 -0400

    remove unnecessary script

--- Entities/Rooms/ritual_scene.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-@export var solution: Array[Item]
-@onready var begin_ritual: Button = %BeginRitual
-
-func _on_begin_ritual_button_up() -> void:
-	pass # Replace with function body.
